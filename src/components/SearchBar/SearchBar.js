0076667b59e04bbd64e423010f4910c05e4159e6
Niraj Bhattarai <nbhattar@ramapo.edu>
2019-04-15 22:18:57 -0400

    Fixed sortByOptions - should have been inside component class

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,25 +1,44 @@
 import React from 'react';
 import './SearchBar.css';
 
-const sortByOptions = {
-  'Best Match': 'best_match',
-  'Highest Rated': 'rating',
-  'Most Reviewed': 'review_count'
-}
+
 
 class SearchBar extends React.Component {
-  renderSortByOptions() {
-    return Object.keys(sortByOptions).map(sortByOption => {
-      let sortByOptionValue = sortByOptions[sortByOption];
-      return <li key={sortByOption}>{sortByOption}</li>;
-    });
+  constructor(props){
+    super(props);
+    this.state = {term : '', location: '', sortBy: 'best_match'}
   }
+  sortByOptions = {
+    'Best Match': 'best_match',
+    'Highest Rated': 'rating',
+    'Most Reviewed': 'review_count'
+  }
+  getSortByClass(sortByOption){
+    if(this.state.sortBy == sortByOption){
+      return 'active'
+    }
+    else{
+      return ''
+    }
+  }
+
+  renderSortByOptions() {
+       return Object.keys(this.sortByOptions).map(sortByOption => {
+           let sortByOptionValue = this.sortByOptions[sortByOption];
+           return (
+               <li key={sortByOptionValue}>
+                 {sortByOption}
+                 </li>
+
+           );
+       });
+}
   render(){
     return(
       <div className='SearchBar'>
         <div className='SearchBar-sort-options'>
           <ul>
-            {this.renderSortByOptions};
+            {this.renderSortByOptions()}
           </ul>
         </div>
         <div className='SearchBar-fields'>
